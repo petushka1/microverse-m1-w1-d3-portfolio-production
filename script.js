e8f3ecd4afd5fc2ce69b3779a133566519916800
jheart-vic <adebowalevictorjheart.com>
2022-05-09 19:49:21 +0100

    Add functionality to menu button

--- script.js
+++ script.js
@@ -3,20 +3,42 @@ const header = document.querySelector('header');
 
 let div = document.createElement('div');
 div.className = 'extended';
-
+let img = document.createElement('img');
+img.className = 'icon'
+img.src = './img/close.svg'
+div.appendChild(img)
+img.addEventListener('click', closed)
+function closed () {
+    div.style.display = 'none'
+}
 let ul =  document.createElement('ul');
 ul.className = 'menu-extended';
 div.appendChild(ul);
 
 let li = document.createElement('li');
-li.textContent = 'About';
+let a = document.createElement('a')
+a.href = '#about'
+a.textContent = 'About';
+a.style.textDecoration = 'none'
+a.style.color ='white'
+li.appendChild(a)
 ul.appendChild(li);
 li = document.createElement('li');
-li.textContent = 'Projects';
+ a = document.createElement('a');
+a.href = '#projects'
+a.textContent = 'Projects';
+a.style.textDecoration = 'none'
+a.style.color ='white'
+li.appendChild(a)
 ul.appendChild(li);
 
 li = document.createElement('li');
-li.textContent = 'Contact';
+a = document.createElement('a');
+a.href = '#contact'
+a.textContent = 'Contact';
+a.style.textDecoration = 'none'
+a.style.color ='white'
+li.appendChild(a)
 ul.appendChild(li);
 
 header.appendChild(div);
