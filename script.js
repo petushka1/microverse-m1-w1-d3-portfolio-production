b7957f5c6e1b702b13859c753bf742622cc5af36
petushka1 <nadya@devery.io>
2022-05-09 23:40:47 +0300

    linter edits

--- script.js
+++ script.js
@@ -1,49 +1,4 @@
-const menu = document.querySelector('.navigation');
 const header = document.querySelector('header');
-
-let div = document.createElement('div');
-div.className = 'extended';
-let img = document.createElement('img');
-img.className = 'icon'
-img.src = './img/close.svg'
-div.appendChild(img)
-img.addEventListener('click', closed)
-
-let ul =  document.createElement('ul');
-ul.className = 'menu-extended';
-div.appendChild(ul);
-
-let li = document.createElement('li');
-let a = document.createElement('a')
-a.href = '#about'
-a.textContent = 'About';
-a.className = 'menu-item';
-li.appendChild(a)
-ul.appendChild(li);
-a.addEventListener('click', closed);
-
-li = document.createElement('li');
- a = document.createElement('a');
-a.href = '#projects'
-a.textContent = 'Projects';
-a.className = 'menu-item';
-li.appendChild(a)
-ul.appendChild(li);
-a.addEventListener('click', closed);
-
-li = document.createElement('li');
-a = document.createElement('a');
-a.href = '#contact'
-a.textContent = 'Contact';
-a.className = 'menu-item';
-li.appendChild(a)
-ul.appendChild(li);
-a.addEventListener('click', closed);
-
-header.appendChild(div);
-
-let btn = document.querySelector('#hmbrg');
-btn.addEventListener('click', opened);
 
 function opened() {
     div.style.display = 'block';
@@ -52,3 +7,47 @@ function opened() {
 function closed () {
     div.style.display = 'none'
 }
+
+const div = document.createElement('div');
+div.className = 'extended';
+const img = document.createElement('img');
+img.className = 'icon';
+img.src = './img/close.svg';
+div.appendChild(img);
+img.addEventListener('click', closed);
+
+const ul = document.createElement('ul');
+ul.className = 'menu-extended';
+div.appendChild(ul);
+
+let li = document.createElement('li');
+let a = document.createElement('a')
+a.href = '#about';
+a.textContent = 'About';
+a.className = 'menu-item';
+li.appendChild(a);
+ul.appendChild(li);
+a.addEventListener('click', closed);
+
+li = document.createElement('li');
+a = document.createElement('a');
+a.href = '#projects';
+a.textContent = 'Projects';
+a.className = 'menu-item';
+li.appendChild(a);
+ul.appendChild(li);
+a.addEventListener('click', closed);
+
+li = document.createElement('li');
+a = document.createElement('a');
+a.href = '#contact';
+a.textContent = 'Contact';
+a.className = 'menu-item';
+li.appendChild(a);
+ul.appendChild(li);
+a.addEventListener('click', closed);
+
+header.appendChild(div);
+
+let btn = document.querySelector('#hmbrg');
+btn.addEventListener('click', opened);
