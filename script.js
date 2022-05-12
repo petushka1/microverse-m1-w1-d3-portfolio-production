3a0bf377d4a331ccaf8d9da58ed8a6737f687654
petushka1 <nadya@devery.io>
2022-05-13 02:06:16 +0300

    errors

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-
-//Dynamic mob menu
+// Dynamic mob menu
 const header = document.querySelector('header');
-var div = document.createElement('div');
-var menu = div;
-var img = document.createElement('img');
-var ul = document.createElement('ul');
-var li = document.createElement('li');
-var a = document.createElement('a');
+let div = document.createElement('div');
+const menu = div;
+let img = document.createElement('img');
+let ul = document.createElement('ul');
+let li = document.createElement('li');
+let a = document.createElement('a');
 
 function opened() {
   div.style.display = 'block';
@@ -64,148 +63,141 @@ const btn = document.querySelector('#hmbrg');
 btn.addEventListener('click', opened);
 
 window.addEventListener('resize', resetMenu);
-
 // dynamic popup script
 
-
-//card template
-
-
-let cardArr = [
+// card template
+const cardArr = [
   {
-    'id': '0',
-    'name': 'Tonic',
-    'spec': ['CANOPY', 'Back End Dev', '2015'],
-    'brief': 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    'image': 'Img'+'\\'+'SnapshootPortfolio3.png',
-    'alt': 'project 1',
-    'technologies': ['html', 'css', 'javaScript'],
-    'liveLink': 'https://petushka1.github.io/microverse-m1-w1-d3-portfolio-production/',
-    'sourceLink': 'https://github.com/petushka1/microverse-m1-w1-d3-portfolio-production'
+    id: '0',
+    name: 'Tonic',
+    spec: ['CANOPY', 'Back End Dev', '2015'],
+    brief: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    image: 'Img\\SnapshootPortfolio3.png',
+    alt: 'project 1',
+    technologies: ['html', 'css', 'javaScript'],
+    liveLink: 'https://petushka1.github.io/microverse-m1-w1-d3-portfolio-production/',
+    sourceLink: 'https://github.com/petushka1/microverse-m1-w1-d3-portfolio-production'
   },
   {
-    'id': '1',
-    'name': 'Tonic',
-    'spec': ['CANOPY', 'Back End Dev', '2015'],
-    'brief': 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    'image': 'Img'+'\\'+'SnapshootPortfolio2.png',
-    'alt': 'project 1',
-    'technologies': ['html', 'css', 'javaScript'],
-    'liveLink': 'https://petushka1.github.io/microverse-m1-w1-d3-portfolio-production/',
-    'sourceLink': 'https://github.com/petushka1/microverse-m1-w1-d3-portfolio-production'
+    id: '1',
+    name: 'Tonic',
+    spec: ['CANOPY', 'Back End Dev', '2015'],
+    brief: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    image: 'Img\\SnapshootPortfolio2.png',
+    alt: 'project 1',
+    technologies: ['html', 'css', 'javaScript'],
+    liveLink: 'https://petushka1.github.io/microverse-m1-w1-d3-portfolio-production/',
+    sourceLink: 'https://github.com/petushka1/microverse-m1-w1-d3-portfolio-production'
   },
   {
-    'id': '2',
-    'name': 'Tonic',
-    'spec': ['CANOPY', 'Back End Dev', '2015'],
-    'brief': 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    'image': 'Img'+'\\'+'SnapshootPortfolio3.png',
-    'alt': 'project 1',
-    'technologies': ['html', 'css', 'javaScript'],
-    'liveLink': 'https://petushka1.github.io/microverse-m1-w1-d3-portfolio-production/',
-    'sourceLink': 'https://github.com/petushka1/microverse-m1-w1-d3-portfolio-production'
+    id: '2',
+    name: 'Tonic',
+    spec: ['CANOPY', 'Back End Dev', '2015'],
+    brief: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    image: 'Img\\SnapshootPortfolio3.png',
+    alt: 'project 1',
+    technologies: ['html', 'css', 'javaScript'],
+    liveLink: 'https://petushka1.github.io/microverse-m1-w1-d3-portfolio-production/',
+    sourceLink: 'https://github.com/petushka1/microverse-m1-w1-d3-portfolio-production'
   },
   {
-    'id': '3',
-    'name': 'Tonic',
-    'spec': ['CANOPY', 'Back End Dev', '2015'],
-    'brief': 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    'image': 'Img'+'\\'+'SnapshootPortfolio.png',
-    'alt': 'project 1',
-    'technologies': ['html', 'css', 'javaScript'],
-    'liveLink': 'https://petushka1.github.io/microverse-m1-w1-d3-portfolio-production/',
-    'sourceLink': 'https://github.com/petushka1/microverse-m1-w1-d3-portfolio-production'
+    id: '3',
+    name: 'Tonic',
+    spec: ['CANOPY', 'Back End Dev', '2015'],
+    brief: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    image: 'Img\\SnapshootPortfolio.png',
+    alt: 'project 1',
+    technologies: ['html', 'css', 'javaScript'],
+    liveLink: 'https://petushka1.github.io/microverse-m1-w1-d3-portfolio-production/',
+    sourceLink: 'https://github.com/petushka1/microverse-m1-w1-d3-portfolio-production'
   },
   {
-    'id': '4',
-    'name': 'Tonic',
-    'spec': ['CANOPY', 'Back End Dev', '2015'],
-    'brief': 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    'description': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
-    'image': 'Img'+'\\'+'SnapshootPortfolio.png',
-    'alt': 'project 1',
-    'technologies': ['html', 'css', 'javaScript'],
-    'liveLink': 'https://petushka1.github.io/microverse-m1-w1-d3-portfolio-production/',
-    'sourceLink': 'https://github.com/petushka1/microverse-m1-w1-d3-portfolio-production'
+    id: '4',
+    name: 'Tonic',
+    spec: ['CANOPY', 'Back End Dev', '2015'],
+    brief: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+    image: 'Img\\SnapshootPortfolio.png',
+    alt: 'project 1',
+    technologies: ['html', 'css', 'javaScript'],
+    liveLink: 'https://petushka1.github.io/microverse-m1-w1-d3-portfolio-production',
+    sourceLink: 'https://github.com/petushka1/microverse-m1-w1-d3-portfolio-production'
   }
 ];
-
 
 const wrapper = document.querySelector('.wrapper');
 
 // card tempate
-
 div = document.createElement('div');
 div.className = 'gridContainer';
 wrapper.appendChild(div);
 const gridContainer = document.querySelector('.gridContainer');
 
 cardArr.forEach((item, i) => {
-let card = document.createElement('div');
-card.className = 'card round';
-gridContainer.appendChild(card);
+  const card = document.createElement('div');
+  card.className = 'card round';
+  gridContainer.appendChild(card);
 
-let snapshot = document.createElement('img');
-snapshot.className = 'snapshot';
-snapshot.src = cardArr[i].image;
-snapshot.alt = cardArr[i].alt;
-card.appendChild(snapshot);
+  let snapshot = document.createElement('img');
+  snapshot.className = 'snapshot';
+  snapshot.src = cardArr[i].image;
+  snapshot.alt = cardArr[i].alt;
+  card.appendChild(snapshot);
 
-let cardContent = document.createElement('div');
-cardContent.className = 'cardContent';
-card.appendChild(cardContent);
+  let cardContent = document.createElement('div');
+  cardContent.className = 'cardContent';
+  card.appendChild(cardContent);
 
-let header = document.createElement('h2');
-header.textContent = cardArr[i].name;
-cardContent.appendChild(header);
+  const header = document.createElement('h2');
+  header.textContent = cardArr[i].name;
+  cardContent.appendChild(header);
 
-let speclist = document.createElement('ul');
-speclist.className = 'spec';
-cardContent.appendChild(speclist);
-let specArr = cardArr[i].spec;
-  for (let j = 0; j < specArr.length; j++) {
-    li = document.createElement('li');
-    li.className = 'specItem';
-    li.textContent = specArr[j];
-    speclist.appendChild(li);
-  }
+  const speclist = document.createElement('ul');
+  speclist.className = 'spec';
+  cardContent.appendChild(speclist);
+  const specArr = cardArr[i].spec;
+    for (let j = 0; j < specArr.length; j++) {
+      li = document.createElement('li');
+      li.className = 'specItem';
+      li.textContent = specArr[j];
+      speclist.appendChild(li);
+    }
 
-let paragraph = document.createElement('p');
-paragraph.textContent = cardArr[i].brief;
-cardContent.appendChild(paragraph);
+  let paragraph = document.createElement('p');
+  paragraph.textContent = cardArr[i].brief;
+  cardContent.appendChild(paragraph);
 
-ul = document.createElement('ul');
-ul.className = 'lng liststyle';
-ul.style.marginBottom = '15px';
-cardContent.appendChild(ul);
-li = document.createElement('li');
-li.className = 'tech';
-li.textContent = cardArr[i].technologies[0];
-ul.appendChild(li);
-li = document.createElement('li');
-li.className = 'tech';
-li.textContent = cardArr[i].technologies[1];
-ul.appendChild(li);
-li = document.createElement('li');
-li.className = 'tech';
-li.textContent = cardArr[i].technologies[2];
-ul.appendChild(li);
-const details  = document.createElement('button');
-details.className = 'btn details';
-details.textContent = 'See project';
-details.name = 'details';
-details.type = 'button';
-details.id = cardArr[i].id;
-cardContent.appendChild(details);
-details.addEventListener('click', openPopup);
+  ul = document.createElement('ul');
+  ul.className = 'lng liststyle';
+  ul.style.marginBottom = '15px';
+  cardContent.appendChild(ul);
+  li = document.createElement('li');
+  li.className = 'tech';
+  li.textContent = cardArr[i].technologies[0];
+  ul.appendChild(li);
+  li = document.createElement('li');
+  li.className = 'tech';
+  li.textContent = cardArr[i].technologies[1];
+  ul.appendChild(li);
+  li = document.createElement('li');
+  li.className = 'tech';
+  li.textContent = cardArr[i].technologies[2];
+  ul.appendChild(li);
+  const details  = document.createElement('button');
+  details.className = 'btn details';
+  details.textContent = 'See project';
+  details.name = 'details';
+  details.type = 'button';
+  details.id = cardArr[i].id;
+  cardContent.appendChild(details);
+  details.addEventListener('click', openPopup);
 });
 
 // Open links when popup buttons clicked
-
 function goToSource(url) {
   if (live) {
       window.open(cardArr[0].liveLink);
@@ -215,12 +207,7 @@ function goToSource(url) {
   }
 }
 
-
-
 //Card Button function
-
-
-
 function openPopup(e) {
   let j = e.target.id;
   let popup = document.createElement('div');
