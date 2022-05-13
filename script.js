906f3f1c2c64384e99f545df67bcdc61a309ee1c
petushka1 <nadya@devery.io>
2022-05-13 20:08:15 +0300

    edit linters errors

--- script.js
+++ script.js
@@ -314,17 +314,15 @@ cardArr.forEach((item, i) => {
 
 // Contact form validation
 const contactForm = document.querySelector('#contactForm');
-const email = contactForm.elements['mail'];
-const userName = contactForm.elements['username'];
-let validationMassege = contactForm.querySelector('#error');
-    validationMassege.style.color = 'red';
-    validationMassege.style.fontSize = '12px'
+const email = contactForm.elements.mail;
+const validationMassege = contactForm.querySelector('#error');
+validationMassege.style.color = 'red';
+validationMassege.style.fontSize = '12px';
 
 contactForm.addEventListener('submit', (event) => {
   let isValid = false;
   if (email.value === email.value.toLowerCase()) {
     isValid = true;
-    console.log(isValid);
   }
   if (isValid === false) {
     event.preventDefault();
@@ -339,7 +337,7 @@ const storage = {
   name: '',
   email: '',
   textMsg: '',
-}
+};
 
 function receiveData() {
   storage.name = document.getElementById('username').value;
